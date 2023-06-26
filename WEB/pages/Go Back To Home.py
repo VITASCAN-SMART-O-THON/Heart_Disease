72966e25c20b7417ab192e226502db81e4d9b45e
VITASCAN-SMART-O-THON <137748780+VITASCAN-SMART-O-THON@users.noreply.github.com>
2023-06-26 15:01:04 +0530

    Update Go Back To Home.py

--- WEB/pages/Go Back To Home.py
+++ WEB/pages/Go Back To Home.py
@@ -7,14 +7,11 @@ import numpy as np
 st.header('Are you sure want to leave this page?')
 
 
-url = 'https://stackoverflow.com'
+url = 'https://mellifluous-hamster-da427b.netlify.app/'
+
+
 st.markdown(f'''
 <a href={url}><button style="background-color:White;  height: 2em; width: 18em; font-size:30px;">Yes</button></a>
 ''',
 unsafe_allow_html=True)
 
-
-st.markdown(f'''
-<a href={url}><button style="background-color:White;height: 2em; font-size:30px; width: 18em">No</button></a>
-''',
-unsafe_allow_html=True)
